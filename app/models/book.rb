2a10aaf9b615b1cb9260ecd5e28a22a688aace8a
Brennan Romance <brennan.romance@gmail.com>
2021-01-13 09:22:30 -0500

    add book search class method and publisher relations/validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -18,17 +18,60 @@ class Book < ApplicationRecord
     class_name: :Author
   })
 
+  belongs_to(:publisher, {
+    foreign_key: :publisher_id,
+    class_name: :Publisher
+  })
+
+
+  def self.search(query, options)
+    collection = []
+    
+    #gather collection of books by provided query
+    if options.title_only == true
+      collection = Book.where("title like ?", "%#{query}%")
+    else
+      author = Author.find_by(first_name: query)
+      if author.nil?
+        author = Author.find_by(last_name: query)
+      end
+      collection = author.books
+    end
+    
+    # filter by provided book attributes in options hash
+    if !!options.book_format_type_id
+      collection = collection.select do |book|
+        matches = book.book_formats.select do |format| 
+          format.book_format_type_id == options.book_format_type_id
+        end
+        matches.length > 0
+      end
+    elsif !options.book_format_physical.nil?
+      collection = collection.select do |book|
+        matches = book.book_formats.select do |format|
+          !BookFormatType.find(format.book_format_type_id).nil?
+        end
+        matches.length > 0
+      end
+    end
+    collection
+  end
+
+
   def book_format_types
     return book_formats.map do |format|
       BookFormatType.find(format.book_format_type_id)
     end
   end
 
+
   def author_name
     return "#{author.last_name}, #{author.first_name}"
   end
 
+
   def average_rating
     ratings.reduce(0) { |acc, rtg| acc + rtg.rating } / ratings.length
   end
+
 end
--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,3 +1,9 @@
 class Publisher < ApplicationRecord
   validates :name, presence: true
+
+  has_many(:books, {
+    foreign_key: :publisher_id,
+    class_name: :Book,
+    dependent: :destroy
+  })
 end
